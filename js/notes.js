82390906f6a18b6bddbaff11a373a01840661da1
dariot <dario.turchetto@gmail.com>
2013-05-23 23:08:21 +0200

    refactoring most of the code (json still has to be used though)

--- js/notes.js
+++ js/notes.js
@@ -9,20 +9,20 @@ $(document).ready(function () {
         content = $("#content"),
         IMG_WIDTH = 15,
         IMG_HEIGHT = 15,
-        IMG_BASE_PATH = "img",
-        EDIT_BTN_PATH = IMG_BASE_PATH + "/edit.png",
-        DELETE_BTN_PATH = IMG_BASE_PATH + "/delete.png";
+        IMG_BASE_PATH = "img/",
+        EDIT_BTN_PATH = IMG_BASE_PATH + "edit.png",
+        DELETE_BTN_PATH = IMG_BASE_PATH + "delete.png";
 
     function load() {
         var i;
         if (!localStorage.notes || localStorage.notes === "") {
             html = "You don't have any notes.";
         } else {
-            savedNotes = JSON.parse(localStorage.notes);
-            for (i = 0; i < savedNotes.length; i++) {
+            savedNotes = localStorage.notes;
+            for (i in savedNotes) {
                 html = "<div class='note'>" +
-                    "<img id='edit" + i + "' src='" + EDIT_BTN_PATH + "' width='" + IMG_WIDTH + "' height='" + IMG_HEIGHT + "' onclick='edit(" + savedNotes[i].id + ")'/>" +
-                    "<img id='delete" + i + "' src='" + DELETE_BTN_PATH + "' width='" + IMG_WIDTH + "' height='" + IMG_HEIGHT + "' onclick='remove(" + savedNotes[i].id + ")'/><br/>" +
+                    "<img id='edit" + i + "' src='" + EDIT_BTN_PATH + "' width='" + IMG_WIDTH + "' height='" + IMG_HEIGHT + "'/>" +
+                    "<img id='delete" + i + "' src='" + DELETE_BTN_PATH + "' width='" + IMG_WIDTH + "' height='" + IMG_HEIGHT + "'/><br/>" +
                     "<b>" + savedNotes[i].title + "</b><br/>" +
                     savedNotes[i].text + "</div><br/><br/>";
             }
@@ -41,7 +41,8 @@ $(document).ready(function () {
         load();
         
         content.on("click", "[id^=edit]", function(ev) {
-            console.log(ev);
+            var id = ev.srcElement.id.replace("edit", "");
+            edit(savedNotes[id]);
         });
         $("[id^=delete]").click();
         $("#removeButton").click(removeAll);
@@ -66,9 +67,9 @@ $(document).ready(function () {
 
     function maxId() {
         var i,
-            max = 0;
+            max = -1;
         if (localStorage.notes) {
-            savedNotes = JSON.parse(localStorage.notes);
+            savedNotes = localStorage.notes;
             for (i = 0; i < savedNotes.length; i++) {
                 if (savedNotes[i].id > max) {
                     max = savedNotes[i].id;
@@ -79,51 +80,27 @@ $(document).ready(function () {
     }
 
     function save(noteId) {
-        var i, o, toAdd, notes = [];
+        var i, o, newNote, notes = [];
 
         if (!noteId || noteId === "") {
             noteId = maxId() + 1;
         }
 
-        if (!localStorage.notes) {
-            notes.push({
-                id: noteId,
-                title: $("#title").val(),
-                text: $("#text").val()
-            });
-            localStorage.notes = JSON.stringify(notes);
-        } else {
-            toAdd = {
-                id: noteId,
-                title: $("#title").val(),
-                text: $("#text").val()
-            };
-            if (noteId > maxId()) {
-                notes = localStorage.notes;
-                o = notes.substring(0, notes.length - 2) + ", " + JSON.stringify(toAdd) + "]}";
-            } else {
-                savedNotes = JSON.parse(localStorage.notes);
-                savedNotes[noteId - 1] = JSON.stringify(toAdd);
-                for (i = 0; i < savedNotes.length; i++) {
-                    if ( i !== (noteId - 1) ) {
-                        savedNotes[i] = JSON.stringify(savedNotes[i]);
-                    }
-                }
-                o = savedNotes;
-            }
-            localStorage.notes.push(o);
-        }
+        localStorage["notes." + noteId + ".id"] = noteId;
+        localStorage["notes." + noteId + ".title"] = $("#title").val();
+        localStorage["notes." + noteId + ".text"] = $("#text").val();
+        
         init();
     }
 
     function edit(noteId) {
-        savedNotes = JSON.parse(localStorage.notes);
+        savedNotes = localStorage.notes;
         html = "<form id='editNoteForm'>" +
-            "Title: <input type='text' id='title' value='" + savedNotes[noteId - 1].title + "' style='width:200px'/><br/>" +
-            "Text: <textarea rows='5' id='text' style='width:200px'>" + savedNotes[noteId - 1].text + "</textarea><br/><br/>" +
+            "Title: <input type='text' id='title' value='" + savedNotes[noteId].title + "' style='width:200px'/><br/>" +
+            "Text: <textarea rows='5' id='text' style='width:200px'>" + savedNotes[noteId].text + "</textarea><br/><br/>" +
             "<table><tr>" +
-            "<td><input type='button' id='saveButton' value='Save note' onclick='save(" + noteId + ")'/></td>" +
-            "<td><input type='button' id='backButton' value='Back' onclick='back()'/></td>" +
+            "<td><input type='button' id='saveButton' value='Save note'/></td>" +
+            "<td><input type='button' id='backButton' value='Back'/></td>" +
             "</tr></table>" +
             "</form>";
         content.html(html);
@@ -132,17 +109,11 @@ $(document).ready(function () {
     function remove(noteId) {
         var i;
         if (confirm("Are you sure you want to delete this note?")) {
-            savedNotes = JSON.parse(localStorage.notes);
+            savedNotes = localStorage.notes;
             if (savedNotes.length === 1) {
                 localStorage.removeItem("notes");
             } else {
                 savedNotes.splice(noteId - 1, 1);
-                for (i = 0; i < savedNotes.length; i++) {
-                    if (i >= noteId - 1) {
-                        savedNotes[i].id = savedNotes[i].id - 1;
-                    }
-                    savedNotes[i] = JSON.stringify(savedNotes[i]);
-                }
                 localStorage.notes = [savedNotes];
             }
             init();
